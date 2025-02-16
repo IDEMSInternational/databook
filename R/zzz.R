f9f7d38bcbe930e71488ae587a2fce7fdc6e9532
lilyclements <lily@idems.international>
2025-02-16 18:56:22 +0000

    remove export

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,5 @@
 #' @keywords internal
 #' @description Runs automatically when the package is loaded and initialises `data_book`.
-#' @export
 .onLoad <- function(libname, pkgname) {
   data_book <<- DataBook$new()
 }
