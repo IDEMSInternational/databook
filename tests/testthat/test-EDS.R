e74bc12676bdd9472acfcca14d301265ae075e93
lilyclements <lily@idems.international>
2025-11-04 18:13:57 +0300

    Update test-EDS.R

--- tests/testthat/test-EDS.R
+++ tests/testthat/test-EDS.R
@@ -2,14 +2,12 @@ test_that("test_EDS_returns_NA_when_unsupported", {
   observed <- c(1, 0, 1, 1, 0)
   predicted <- c(1, 0, 1, 0, 0)
   
-  result <- EDS(
+  expect_warning(
+    EDS(
     x = observed,
     y = predicted,
     frcst.type = "categorical",
     obs.type = "categorical"
-  )
+  ), "EDS not supported for this forecast/observation combination. Returning NA.")
   
-  # EDS is likely unsupported for categorical forecasts
-  expect_true(is.numeric(result))
-  expect_true(is.na(result))
 })
